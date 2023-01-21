5ad69b97b88fcfcc49b12d5866e8b9973d6a614a
Jose Lavado <jose.lavado2016@gmail.com>
2023-01-21 11:02:51 -0500

    added new js file

---
+++ example.js
@@ -0,0 +1,19 @@
+
+    //This is the first line of code
+    var number=5; //in line comment
+    /* This is line1
+    This is line2
+    This is line3 */
+    var number2=9;
+    console.log("Hello World from JavaSCript");
+    console.log(number2);
+    console.log(number);
+    var name= "Joe";
+    const pi = 3.14159
+    console.log(pi)
+
+
+
+
+
+
